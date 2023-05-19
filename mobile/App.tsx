e773018d8aea6eb8fc01557c210e549430630e19
Helena-Antoni <helenaantoni03@gmail.com>
2023-05-19 16:03:03 -0300

    Aula 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
 
 import {
   useFonts,
@@ -10,7 +11,11 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
-import React from 'react'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -25,11 +30,35 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-700"
+      className="relative flex-1 items-center bg-gray-700 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
-      <Text className="font-body text-5xl text-gray-50">se fude</Text>
-      <Text className="font-alt text-5xl text-gray-50">Rocketseat</Text>
+      <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="font-tile text-center text-2xl leading-tight text-gray-50">
+            Sua Cásula do Tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da usa jornada e compartilhe (se
+            quiser) com o mundo !
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
---
+++ server/src/routes/memories.ts
@@ -0,0 +1,98 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { request } from 'http'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+  /** ******** GET *********/
+  app.get('/memories/:id', async (request) => {
+    // const { id } = request.params
+
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+    return memory
+  })
+
+  /** ******** POST *********/
+  app.post('/memories/', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '1f663aff-9ca7-4435-93bc-ff68451a9310',
+      },
+    })
+    return memory
+  })
+
+  /** ******** PUT *********/
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+    return memory
+  })
+  /** ******** DELETE *********/
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
